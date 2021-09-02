536e8c581cb4c280a96fd6d4ab1127b304461445
weevil <abhinavparagmishra23@gmail.com>
2021-09-03 02:08:31 +0530

    Create IMDB crawler and crawl the site for movies data

---
+++ server/crawler.js
@@ -0,0 +1,73 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+class Crawler {
+  #fetchedData = []; // private field
+
+  getFetchedData() {
+    return this.#fetchedData;
+  }
+
+  #extractFieldsFromHTML(html) {
+    const $ = cheerio.load(html);
+    $(".lister-item-content").each((index, element) => {
+
+      const loadedContent = $(element);
+      const rank = Number(loadedContent.find(".lister-item-index").text().trim().replace(/,/g,""));
+      const title = loadedContent.find("h3>a").text().trim();
+      const year = loadedContent.find(".lister-item-year").text().trim().substr(1, 4);
+      const certificate = loadedContent.find(".certificate").text().trim();
+      const runtime = loadedContent.find(".runtime").text().trim();
+      let genre = loadedContent.find(".genre").text().trim();
+      genre = genre.split(",");
+      const imdbRating = loadedContent.find(".ratings-imdb-rating>strong").text().trim();
+      const metascore = loadedContent.find(".metascore").text().trim();
+      const additionalInfo = loadedContent.children("p");
+      const description = additionalInfo.eq(1).text().trim();
+      let people = additionalInfo.eq(2).text().trim().replace(/\n/g, "");
+      let [directors, stars] = people.split("|").map(el => el.trim());
+      directors = directors.substring(9).split(",").map(el => el.trim());
+      stars = stars.substring(6).split(",").map(el => el.trim());
+      this.#fetchedData.push({
+        rank, title, year, certificate, runtime, genre, imdbRating, metascore, description, directors, stars
+      })
+    })
+  }
+
+  async makeRequestAndExtractFields(startMovieNumber = 1, movieCount = 10) {
+    return new Promise((resolve, reject) => {
+      request(`https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=${movieCount}&start=${startMovieNumber}&ref_=adv_nxt`,
+        (error, response, html) => {
+          if (!error && response.statusCode === 200) {
+            this.#extractFieldsFromHTML(html);
+            resolve({ message: "Extraction successful" });
+          } else {
+            reject(error);
+          }
+        })
+    })
+  }
+
+  async crawl() {
+    let moviesPerPage = 250
+    for(let i = 0; i < 4; i++){
+      await this.makeRequestAndExtractFields( moviesPerPage*i + 1, moviesPerPage );
+    }
+    this.#saveDataToJsonFile();
+  }
+
+  #saveDataToJsonFile() {
+    fs.writeFile("movies.json", JSON.stringify(this.#fetchedData, null, 2), function (err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("Writing to file was successfull");
+      }
+    })
+  }
+
+}
+
+const obj = new Crawler();
+obj.crawl();
